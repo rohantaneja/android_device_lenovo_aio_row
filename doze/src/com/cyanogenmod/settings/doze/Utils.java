3668c5e959e00df96e009b64a67ff6ab84f06e80
Rohan Taneja <rohantaneja63@gmail.com>
2017-07-16 18:10:26 +0530

    doze: fix compilation error & gesture support

--- doze/src/com/cyanogenmod/settings/doze/Utils.java
+++ doze/src/com/cyanogenmod/settings/doze/Utils.java
@@ -1,6 +1,5 @@
 /*
  * Copyright (c) 2015 The CyanogenMod Project
- *               2017 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,7 +18,7 @@ package com.cyanogenmod.settings.doze;
 
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.UserHandle;
 import android.support.v7.preference.PreferenceManager;
 import android.provider.Settings;
@@ -34,41 +33,21 @@ public final class Utils {
 
     private static final String DOZE_INTENT = "com.android.systemui.doze.pulse";
 
-    protected static final String CATEG_PROX_SENSOR = "proximity_sensor";
-
+    protected static final String AMBIENT_DISPLAY_KEY = "doze_enabled";
     protected static final String GESTURE_PICK_UP_KEY = "gesture_pick_up";
     protected static final String GESTURE_HAND_WAVE_KEY = "gesture_hand_wave";
     protected static final String GESTURE_POCKET_KEY = "gesture_pocket";
 
+    public static final Uri DOZE_ENABLED_URI = Settings.Secure.getUriFor(DOZE_ENABLED);
+
     protected static void startService(Context context) {
         if (DEBUG) Log.d(TAG, "Starting service");
-        context.startServiceAsUser(new Intent(context, DozeService.class),
-                UserHandle.CURRENT);
+        context.startService(new Intent(context, DozeService.class));
     }
 
     protected static void stopService(Context context) {
         if (DEBUG) Log.d(TAG, "Stopping service");
-        context.stopServiceAsUser(new Intent(context, DozeService.class),
-                UserHandle.CURRENT);
-    }
-
-    protected static void checkDozeService(Context context) {
-        if (isDozeEnabled(context) && sensorsEnabled(context)) {
-            startService(context);
-        } else {
-            stopService(context);
-        }
-    }
-
-    protected static boolean getProxCheckBeforePulse(Context context) {
-        try {
-            Context con = context.createPackageContext("com.android.systemui", 0);
-            int id = con.getResources().getIdentifier("doze_proximity_check_before_pulse",
-                    "bool", "com.android.systemui");
-            return con.getResources().getBoolean(id);
-        } catch (PackageManager.NameNotFoundException e) {
-            return false;
-        }
+        context.stopService(new Intent(context, DozeService.class));
     }
 
     protected static boolean isDozeEnabled(Context context) {
@@ -77,8 +56,14 @@ public final class Utils {
     }
 
     protected static boolean enableDoze(boolean enable, Context context) {
-        return Settings.Secure.putInt(context.getContentResolver(),
-                DOZE_ENABLED, enable ? 1 : 0);
+        boolean dozeEnabled = Settings.Secure.putInt(context.getContentResolver(),
+                              DOZE_ENABLED, enable ? 1 : 0);
+        if (enable) {
+            startService(context);
+        } else {
+            stopService(context);
+        }
+        return dozeEnabled;
     }
 
     protected static void launchDozePulse(Context context) {
