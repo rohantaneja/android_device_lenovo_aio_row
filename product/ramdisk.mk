0bd9138dc36e8c3e409f149ba3894a6c547a1d1a
Rohan Taneja <rohantaneja63@gmail.com>
2017-07-16 14:14:35 +0530

    products: cleanup & remove duplicate product call

--- product/ramdisk.mk
+++ product/ramdisk.mk
@@ -1,14 +1,14 @@
 # Ramdisk
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/rootdir/enableswap.sh:root/enableswap.sh \
-    $(LOCAL_PATH)/rootdir/fstab.mt6752:root/fstab.mt6752 \
-    $(LOCAL_PATH)/rootdir/init.modem.rc:root/init.modem.rc \
-    $(LOCAL_PATH)/rootdir/init.mt6752.usb.rc:root/init.mt6752.usb.rc \
-    $(LOCAL_PATH)/rootdir/init.mt6752.rc:root/init.mt6752.rc \
-    $(LOCAL_PATH)/rootdir/init.project.rc:root/init.project.rc \
-    $(LOCAL_PATH)/rootdir/ueventd.mt6752.rc:root/ueventd.mt6752.rc
+PRODUCT_PACKAGES += \
+    enableswap.sh \
+    fstab.mt6752 \
+    init.modem.rc \
+    init.mt6752.usb.rc \
+    init.mt6752.rc \
+    init.project.rc \
+    ueventd.mt6752.rc
 
 # Spectrum additions
-PRODUCT_COPY_FILES += \
-     $(LOCAL_PATH)/rootdir/init.spectrum.sh:root/init.spectrum.sh \
-    $(LOCAL_PATH)/rootdir/init.spectrum.rc:root/init.spectrum.rc
+PRODUCT_PACKAGES += \
+    init.spectrum.sh \
+    init.spectrum.rc
